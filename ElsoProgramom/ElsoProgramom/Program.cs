728577c3c59da93066667ae5aa85ec9a37d814aa
Krisztina Veszelszki <krisztina.veszelszki@gmail.com>
2017-10-11 17:35:07 +0000

    metodus

--- ElsoProgramom/ElsoProgramom/Program.cs
+++ ElsoProgramom/ElsoProgramom/Program.cs
@@ -14,15 +14,29 @@ namespace ElsoProgramom
         {
             Kiir();
             Kiir2();
-            int c=Osszead(5, 7);
-            Console.WriteLine("a két szám eredménye: {0}", c);
+            int d=Osszead(5, 7,9);
+            Console.WriteLine("a három szám eredménye: {0}", d);
+            int x = Szoroz(3, 8);
+            Console.WriteLine("a két szám szorzata: {0} {1}", x,d);
             Console.ReadLine();
+
         }
 
-        static int Osszead(int a, int b)
+
+        // ez egy metódus, az a neve, hogy Osszead, három paramétere van mindhárom egész szám (int). 
+        //A metódusnak van egy visszatérési értéke, tehát valami kijön ebből a hívásból.
+        //
+
+        static int Osszead(int a, int b, int c)
         {
-            return a + b;
+            return a + b + c;
         }
+
+        //a metódus szignatúrája az első sor, melynek tartalma a static, az int (visszatérési érték típusa)
+        // az Osszead a metódus neve, ezt követően a paramétereket tartalmazó zárójel van
+        //a paraméterek típusból és névből állnak, vesszővel vannak elválasztva
+        //a metódus szignatúráját az implementácó követi kapcsos zárójelben és amennyiben adtunk meg visszatérési 
+        //típust a szignatúrában, akkor a return-nek legalább egyszer szerepelnie kell
 
         static void Kiir()
         {
@@ -33,5 +47,12 @@ namespace ElsoProgramom
         {
             Console.WriteLine(szoveg, "alma", 2);
         }
+
+        static int Szoroz (int a, int b)
+        {
+            int c = a * b;
+            return c;
+        }
+
     }
 }
